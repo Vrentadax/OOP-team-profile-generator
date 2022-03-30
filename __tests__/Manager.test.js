4c17e3f5dd0728f29a7b8d5d60e34b512266dd13
Vrentadax <vrentadax@outlook.com>
2022-03-30 17:31:46 -0600

    wrote tests and constructor for Manager, tests come back successful

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,19 @@
+const Manager = require('../lib/Manager');
+
+test('Can set the office number', () => {
+    const officeNo = 1;
+    const test = new Manager('John Doe', 123, 'bmilner88@gmail.com', officeNo);
+    expect(test.officeNo).toBe(officeNo);
+});
+
+test('Can use getOfficeNumber() to return the office number', () => {
+    const officeNo = 1;
+    const test = new Manager('John Doe', 123, 'bmilner88@gmail.com', officeNo);
+    expect(test.getOfficeNumber()).toBe(officeNo);
+});
+
+test(`Can use getRole() to return 'Manager'`, () => {
+    const role = 'Manager';
+    const test = new Manager('John Doe', 123, 'bmilner88@gmail.com', 1);
+    expect(test.getRole()).toBe(role);
+});

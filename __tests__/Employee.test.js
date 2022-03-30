61c656ac4f2a717f29adae5af485e56d5e1d50e3
Vrentadax <vrentadax@outlook.com>
2022-03-30 17:23:37 -0600

    Employee obj should be complete, tests all come outsuccessful

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -22,3 +22,27 @@ it('Can set the email address', () => {
     const test = new Employee('John Doe', 123, testEmail);
     expect(test.email).toBe(testEmail);
 });
+
+it('Can use getName() to return the name', () => {
+    const testName = 'John Doe';
+    const test = new Employee(testName);
+    expect(test.getName()).toBe(testName);
+});
+
+it('Can use getId() to return the ID', () => {
+    const testId = 123;
+    const test = new Employee('John Doe', testId);
+    expect(test.getId()).toBe(testId);
+});
+
+it('Can use getEmail() to return the email address', () => {
+    const testEmail = 'email@email.com';
+    const test = new Employee('John Doe', 123, testEmail);
+    expect(test.getEmail()).toBe(testEmail);
+});
+
+it(`Can use getRole() to return 'Employee'`, () => {
+    const role = 'Employee';
+    const test = new Employee('John Doe', 123, 'email@email.com');
+    expect(test.getRole()).toBe(role);
+});
